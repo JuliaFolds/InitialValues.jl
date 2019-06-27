bc7f1b16f19e7987fcb0f6b463ca814a2fb34f1b
Takafumi Arakaki <aka.tkf@gmail.com>
2019-06-26 20:42:35 -0700

    Add methods for a few methods

--- src/UniversalIdentity.jl
+++ src/UniversalIdentity.jl
@@ -1,5 +1,31 @@
 module UniversalIdentity
 
-greet() = print("Hello World!")
+using Requires
+
+struct Identity{OP} end
+
+Identity(::OP) where OP = Identity{OP}()
+
+itypeof(f) = typeof(Identity(f))
+
+Base.:*(::itypeof(*), x) = x
+Base.:+(::itypeof(+), x) = x
+Base.:&(::itypeof(&), x) = x
+Base.:|(::itypeof(|), x) = x
+Base.min(::itypeof(min), x) = x
+Base.max(::itypeof(max), x) = x
+Base.add_sum(::itypeof(Base.add_sum), x) = x
+Base.mul_prod(::itypeof(Base.mul_prod), x) = x
+
+# Disambiguation:
+Base.min(::itypeof(min), ::Missing) = missing
+Base.max(::itypeof(max), ::Missing) = missing
+
+function __init__()
+    @require BangBang="198e06fe-97b7-11e9-32a5-e1d131e6ad66" begin
+        BangBang.push!!(::itypeof(BangBang.push!!), x) = [x]
+        BangBang.append!!(::itypeof(BangBang.append!!), x) = x
+    end
+end
 
 end # module
