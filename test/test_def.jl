1d252338f28f151d7e1de06a5c68fd766492ba20
Takafumi Arakaki <aka.tkf@gmail.com>
2019-06-30 15:57:46 -0700

    Test at-def with non-function callable

--- test/test_def.jl
+++ test/test_def.jl
@@ -16,4 +16,19 @@ end
     @test add(Identity(add), :x) == :x
 end
 
+module NonFunction
+    using UniversalIdentity: @def
+    struct Add end
+    const add = Add()
+    add(x, y) = x + y
+    @def add
+end
+
+@testset "NonFunction" begin
+    add = NonFunction.add
+    @test !(add isa Function)
+    @test hasidentity(add)
+    @test add(Identity(add), :x) == :x
+end
+
 end  # module
