54f1f0b43aff57aa35fd624132a901b88708007d
Takafumi Arakaki <aka.tkf@gmail.com>
2019-10-13 11:59:08 -0700

    Remove Setfield from test dependency

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -2,14 +2,12 @@ module TestAqua
 
 using Aqua
 using InitialValues
-using Setfield
 using BangBang
 
 Aqua.test_all(InitialValues)
 
 Aqua.test_ambiguities(
     [InitialValues, BangBang, Base];
-    exclude=[Base.get, Setfield.set, Setfield.modify],
 )
 
 end  # module
