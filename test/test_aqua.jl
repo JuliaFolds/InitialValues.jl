ad295dd0ad21024161a2d42e82eb37e02233dad9
Takafumi Arakaki <aka.tkf@gmail.com>
2019-12-09 18:17:23 -0800

    Exclude ambiguities from DataStructures in `Aqua.test_ambiguities`

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,5 +1,6 @@
 module TestAqua
 
+import Setfield
 using Aqua
 using InitialValues
 using BangBang
@@ -8,6 +9,7 @@ Aqua.test_all(InitialValues)
 
 Aqua.test_ambiguities(
     [InitialValues, BangBang, Base];
+    exclude = [Base.get, Setfield.set, Setfield.modify],
 )
 
 end  # module
