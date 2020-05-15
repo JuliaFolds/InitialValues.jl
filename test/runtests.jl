bf7c74aafe02b74954ce0d98cda1a23d414e7110
Takafumi Arakaki <aka.tkf@gmail.com>
2020-05-14 20:20:36 -0700

    Do not promote to Any (#27)
    * Do not promote to Any
    
    Before:
    
        julia> push!!([Init(+)], 0)
        2-element Array{Any,1}:
          Init(+)
         0
    
    After:
    
        julia> push!!([Init(+)], 0)
        2-element Array{Union{InitialValues.InitialValueOf{typeof(+)}, Int64},1}:
          Init(+)
         0
    
    * Skip ambiguity tests on Julia 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,9 @@ using Test
 
 @testset "$file" for file in sort([file for file in readdir(@__DIR__) if
                                    match(r"^test_.*\.jl$", file) !== nothing])
+    # Ambiguity tests fail on Julia 1.0 as of writing.
+    VERSION < v"1.1" && file == "test_aqua.jl" && continue
+
     include(file)
 end
 
