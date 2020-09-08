c8e3686c6a4f874cf8251e60e2c09f9d21673b84
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-07 19:31:18 -0700

    Update to Aqua 0.5

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -12,4 +12,9 @@ Aqua.test_ambiguities(
     exclude = [Base.get, Setfield.set, Setfield.modify],
 )
 
+@testset "Compare test/Project.toml and test/environments/main/Project.toml" begin
+    @test Text(read(joinpath(@__DIR__, "Project.toml"), String)) ==
+          Text(read(joinpath(@__DIR__, "environments", "main", "Project.toml"), String))
+end
+
 end  # module
